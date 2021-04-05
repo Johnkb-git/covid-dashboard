86377917e871c280816e6150e610cf26195384c2
Johnkb-git <kbzhang@gwmail.gwu.edu>
2021-04-05 18:33:52 -0400

    first commit

---
+++ src/test/java/edu/gwu/ood/dashboard/CovidDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gwu.ood.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
