4c6d13b2a8c29920c3e8b253260c4fdcbf124d69
Johnkb-git <kbzhang@gwmail.gwu.edu>
2021-04-07 18:28:24 -0400

    remove old java files

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#    return 'Hello World'
 
 @app.route('/')
-def hello_world():
-   return 'Hello World'
+def index():
+    return flask.render_template("login.html")
 
 if __name__ == '__main__':
    app.run()
