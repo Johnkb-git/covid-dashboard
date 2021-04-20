bb8d3595b639f1f87a04a229209ba06c417f8810
Johnkb-git <kbzhang@gwmail.gwu.edu>
2021-04-19 20:22:48 -0400

    final version

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import flask
 import database
+import get_news
+import get_data
 
 app = flask.Flask(__name__)
 
 
-# @app.route('/')
-# def hello_world():
-#    return 'Hello World'
 @app.route('/')
 def index():
     return flask.redirect(flask.url_for('login'))
@@ -17,73 +16,58 @@ def login():
     if flask.request.method == 'POST':
         username = flask.request.form['username']
         password = flask.request.form['password']
-        if database.verify_user(username,password):
-            return flask.redirect(flask.url_for('main_page'))
+        if database.verify_user(username, password):
+            return flask.redirect(flask.url_for('main_page', user_id=username))
         else:
             error = 'Invalid username or password. Please try again!'
-            return flask.render_template("login.html", error = error)
-        # some code contact with database
-
+            return flask.render_template("login.html", error=error)
     return flask.render_template("login.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if flask.request.method == 'POST':
-        print("get post request")
         username = flask.request.form['username']
         password = flask.request.form['password']
-        print(username)
-        print(password)
-
-        # some code contact with database
-
     return flask.render_template("create-account.html")
 
 
-@app.route('/mainpage', methods=['GET'])
-def main_page():
-    # some code contact with database
-
-    # some code reform the main page html based on the user id
-    userid = "1"
-    return flask.render_template("mainpage.html", userid=userid)
+@app.route('/mainpage/?<string:user_id>', methods=['GET'])
+def main_page(user_id):
+    return flask.render_template("mainpage.html", userid=user_id)
 
 
-@app.route('/list/medicine/<user_id>', methods=['GET'])
+@app.route('/mainpage/list/medicine/<user_id>', methods=['GET', 'POST'])
 def medicine(user_id):
-    # some code contact with database
-
-    # some code reform the main page html based on the user id
+    medicine_list = database.get_medicine('medicine', user_id)
     table_name = "Medicine records"
-    return flask.render_template("medicine_table.html", table_name=table_name)
+    return flask.render_template("medicine_table.html", table_name=table_name, medicine_list=medicine_list)
 
 
-@app.route('/list/takeout/<user_id>', methods=['GET'])
+@app.route('/mainpage/list/takeout/<user_id>', methods=['GET'])
 def takeout(user_id):
-    # some code contact with database
-
-    # some code reform the main page html based on the user id
     table_name = "Takeout"
     return flask.render_template("takeout_table.html", table_name=table_name)
 
 
-@app.route('/list/news/<user_id>', methods=['GET'])
+@app.route('/mainpage/list/news/<user_id>', methods=['GET'])
 def news(user_id):
-    # some code contact with database
-
-    # some code reform the main page html based on the user id
     table_name = "News"
-    return flask.render_template("news_table.html", table_name=table_name)
+    data = get_data.get_data()
+    news_list = get_news.get_news()
+    return flask.render_template("news_table.html", table_name=table_name, data=data, news_list=news_list)
 
 
-@app.route('/list/trips/<user_id>', methods=['GET'])
+@app.route('/mainpage/list/trips/<user_id>', methods=['GET'])
 def trips(user_id):
-    # some code contact with database
-
-    # some code reform the main page html based on the user id
     table_name = "Trips"
     return flask.render_template("trip_table.html", table_name=table_name)
+
+
+@app.route('/post/medicine/<user_id>', methods=['POST'])
+def post_medicine(user_id):
+    database.post_medicine(flask.request.form, user_id)
+    return flask.redirect(flask.url_for('medicine', user_id=user_id))
 
 
 if __name__ == '__main__':
